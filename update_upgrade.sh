97ec4814269a9c1af2983aaa2fbb1219a3474fbc
Sochamroun <267657641+Sochamroun@users.noreply.github.com>
2026-09-14 06:50:55 +0700

    Create update_upgrade.sh

---
+++ update_upgrade.sh
@@ -0,0 +1,22 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "Starting Termux Setup install software..."
+# Server 
+termux-change-repo
+# update 
+yes | pkg update && yes | pkg upgrade
+
+# Install packages
+yes | pkg install python git jq tmux wget iproute2 unzip fish nano nodejs nmap -y
+
+# Fix fish config folder (important)
+mkdir -p ~/.config/fish
+
+# Set fish shell (safe)
+chsh -s fish || echo "⚠️ Cannot change shell (ignore if error)"
+
+# Fix PATH
+grep -qxF 'set -U fish_user_paths $fish_user_paths $HOME' ~/.config/fish/config.fish || \
+echo 'set -U fish_user_paths $fish_user_paths $HOME' >> ~/.config/fish/config.fish
+
+echo "THANK You"
